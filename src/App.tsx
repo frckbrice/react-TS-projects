b0cddca332153a0a87759bb1d72f49a8c6e35a49
frckbrice <maebrie2017@gmail.com>
2023-10-03 08:44:21 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Heading from "./components/Heading";
+import { Section } from "./components/Section";
+
+const App = () => {
+  return (
+    <>
+      <Heading title={"Title"} />
+      <Section title={"Section Subtitle"}>This is the section. </Section>
+    </>
+  );
+};
+
+export default App;
